03f2c30bffb37cc8256f23eb95427874d91661eb
Siksna <ulfsssiksna@gmail.com>
2023-03-07 09:19:36 +0200

    Refaktoreta programma, ievesta metode mestKaulinu

--- src/Git_kaulins.java
+++ src/Git_kaulins.java
@@ -1,13 +1,15 @@
 import java.util.Random;
 
 public class Git_kaulins {
-
+static void mestKaulinu(){
+	int skaitlis;
+	Random rand = new Random();
+	skaitlis = rand.nextInt(6)+1;
+	System.out.println("Uzkrita skaitlis: "+skaitlis);
+}
 	public static void main(String[] args) {
 		
-int skaitlis;
-Random rand = new Random();
-skaitlis = rand.nextInt(6)+1;
-System.out.println("Uzkrita skaitlis: "+skaitlis);
+mestKaulinu();
 	}
 
 }
