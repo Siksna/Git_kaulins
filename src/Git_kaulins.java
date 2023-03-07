c26379a4814cce4b3e0715ff9c63b7f63787a389
Siksna <ulfsssiksna@gmail.com>
2023-03-07 09:01:32 +0200

    Realizeta iespeja 1x mest kaulinu

--- src/Git_kaulins.java
+++ src/Git_kaulins.java
@@ -1,9 +1,13 @@
+import java.util.Random;
 
 public class Git_kaulins {
 
 	public static void main(String[] args) {
 		
-
+int skaitlis;
+Random rand = new Random();
+skaitlis = rand.nextInt(6)+1;
+System.out.println("Uzkrita skaitlis: "+skaitlis);
 	}
 
 }
