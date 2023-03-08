1ac5d15b1a9885c152d511655cc92fffeafae3c5
Siksna <ulfsssiksna@gmail.com>
2023-03-08 10:10:26 +0200

    izveidota metode kas uzmet kaulinu

--- src/Git_kaulins.java
+++ src/Git_kaulins.java
@@ -1,15 +1,25 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Git_kaulins {
-static void mestKaulinu(){
-	int skaitlis;
-	Random rand = new Random();
-	skaitlis = rand.nextInt(6)+1;
-	System.out.println("Uzkrita skaitlis: "+skaitlis);
-}
+
+	static void mestKaulinu(int reizes){
+		Random rand = new Random();
+		int skaitlis;
+		for(int i=1;i<=reizes;i++){
+			skaitlis = rand.nextInt(6)+1;
+			System.out.println("Uzkrita skaitlis: "+skaitlis);	
+	}
+	}
 	public static void main(String[] args) {
-		
-mestKaulinu();
+	
+int reizes;
+
+Scanner dati = new Scanner(System.in);
+System.out.println("Cik reizes mest kaulinu?");
+reizes = dati.nextInt();
+mestKaulinu(reizes);
+	}
 	}
 
-}
+
