9f38ef07fcd29de17b10425b02741443fba15b4d
Siksna <ulfsssiksna@gmail.com>
2023-03-08 09:58:49 +0200

    Pievienots metiens ar reizu skaitu

--- src/Git_kaulins.java
+++ src/Git_kaulins.java
@@ -1,13 +1,20 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Git_kaulins {
 
 	public static void main(String[] args) {
 		
-int skaitlis;
+int skaitlis, reizes;
 Random rand = new Random();
+Scanner dati = new Scanner(System.in);
+System.out.println("Cik reizes mest kaulinu?");
+reizes = dati.nextInt();
+for(int i=1;i<=reizes;i++){
 skaitlis = rand.nextInt(6)+1;
 System.out.println("Uzkrita skaitlis: "+skaitlis);
+	}
+dati.close();
 	}
 
 }
