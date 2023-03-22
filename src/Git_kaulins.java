08eb853fde02b46c7faeb9c50b7f709f75ddb495
Viljams Trukšāns <vilijams25@gmail.com>
2023-03-22 08:47:36 +0200

    Optimizets kods un ievade/izvade izveidota ar JOptionPane

--- src/Git_kaulins.java
+++ src/Git_kaulins.java
@@ -1,24 +1,20 @@
 import java.util.Random;
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 public class Git_kaulins {
 
 	static void mestKaulinu(int reizes){
 		Random rand = new Random();
-		int skaitlis;
 		for(int i=1;i<=reizes;i++){
-			skaitlis = rand.nextInt(6)+1;
-			System.out.println("Uzkrita skaitlis: "+skaitlis);	
+			JOptionPane.showMessageDialog(null,"Uzkrita: "+(rand.nextInt(6)+1));	
 	}
 	}
 	public static void main(String[] args) {
 	
-int reizes;
-
-Scanner dati = new Scanner(System.in);
-System.out.println("viss guud");
-System.out.println("Cik reizes mest kaulinu?");
-reizes = dati.nextInt();
+int reizes = Integer.parseInt(JOptionPane.showInputDialog(null, "cik reizes metis kaulinu?"));
+JOptionPane.showMessageDialog(null, "viss guud!");
 mestKaulinu(reizes);
 	}
 	}
